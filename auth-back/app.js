3413c631134e0c9b7c8bf91ab91f4ac21f3e5cf7
VictorCesp <victor.cespedes@usa.edu.co>
2024-03-19 02:54:11 -0500

    rutas

--- auth-back/app.js
+++ auth-back/app.js
@@ -5,7 +5,17 @@ const mysql = require("mysql");
 
 require("dotenv").config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3100;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/signup", require("./routes/signup"));
+app.use("/api/login", require("./routes/login"));
+app.use("/api/user", require("./routes/user"));
+app.use("/api/signout", require("./routes/signout"));
+app.use("/api/todos", require("./routes/todos"));
+app.use("/api/refreshToken", require("./routes/refreshToken"));
 
 app.get("/", (req,res) => {
    res.send("Mensaje");
