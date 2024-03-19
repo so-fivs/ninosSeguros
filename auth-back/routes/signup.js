42fd7ef469c459b60f8e6e2f9f365b690f70b16a
VictorCesp <victor.cespedes@usa.edu.co>
2024-03-19 03:19:32 -0500

    connection back & front v1 (error 400)

--- auth-back/routes/signup.js
+++ auth-back/routes/signup.js
@@ -1,6 +1,21 @@
 const router = require("express").Router();
+const { jsonResponse } = require("../lib/jsonResponse")
 
-router.get("/", (req,res) => {
+router.post("/", (req,res) => {
+    //cuando creo un nuevo usuario, voy a esperar los datos
+    const {name,username,password} = req.body;
+
+    //error si no estan los datos
+    if(!!name || !!username || !!password){ // si no hay nada en x dato, envio una respuesta
+        return res.status(400).json(
+            jsonResponse(400, {
+                error: "Campos requeridos",
+            })
+        );
+    }
+
+    //crear el usuario
+    res.status(200).json(jsonResponse(200,{mensaje: "Usuario creado correctamente"}));
     res.send("signup");
 });
 
--- auth-front/src/routes/signup.tsx
+++ auth-front/src/routes/signup.tsx
@@ -2,21 +2,47 @@ import Default from "../templates/default";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../auth/Authenti";
 import { useState } from "react";
+import { API_URL } from "../auth/constants";
 
 export default function Signup() {
 
-    const [name, setName] = useState("");
+    const [name, setName]     = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const auth = useAuth();
 
-    if(auth.isAuthenticated){
+    if( auth.isAuthenticated ){
         return <Navigate to = "/directory"/>;
     }
 
+    async function handleSubmit( e : React.FormEvent<HTMLFormElement> ){
+        e.preventDefault();
+        try{
+            const response = await fetch( `${API_URL}/signup`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name,
+                    username,
+                    password,
+                }),
+            });
+
+            if (response.ok){
+                console.log("Usuario creado OK");
+            }else{
+                console.log("Algo ocurrio");
+            }
+
+        }catch (error) {
+            console.log(error);
+        }
+    }
     return (
         <Default>
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
                 <h1>Signup</h1>
                 <label>Nombre:</label>
                 <input type="text"
