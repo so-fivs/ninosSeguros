c9ca1e719ea7df6175a14d2fe8b07bd5a2a3df54
Sofi <Sofivesi5@gmail.com>
2024-03-21 14:51:12 -0500

    creacion y modificacion de pagina publicacion

---
+++ auth-front/src/routes/publicacion.tsx
@@ -0,0 +1,13 @@
+import Portal from "../templates/portal";
+
+
+export default function Perfil() {
+    return (
+        <Portal>
+            <div className="dashboard">
+                <h1>Perfil de "usuario"</h1>
+
+            </div>
+        </Portal>
+    );
+}
